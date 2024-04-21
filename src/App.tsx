403990b1bec69b4a4ef1025c22fc129393318e94
Nicole Mascaretti <nsmascaretti@gmail.com>
2024-04-20 22:09:59 -0300

    testando husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Home from "./screens/Home.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <h1>🚀 Vite React Template 🚀</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <h1>🚀 Vite React Template 🚀</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <Home></Home>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
